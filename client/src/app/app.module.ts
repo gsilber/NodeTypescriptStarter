cd1633a53acb62867824cd8198382cbf9155b16d
Marc Bolinas <bolinas.marc@gmail.com>
2019-11-08 13:37:51 -0500

    added store and library views

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+import { LibraryComponent } from './library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
