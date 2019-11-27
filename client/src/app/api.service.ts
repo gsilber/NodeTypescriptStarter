7be51f24d70b0b9f4133f383b43eadb2bda0c742
Marc Bolinas <bolinas.marc@gmail.com>
2019-11-26 21:43:18 -0500

    signIn, signOut, isAuth, and getUsers

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable, of, Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,9 @@ export class ApiService {
   //private headers = new Headers({'Content-Type': 'application/json'});
   //TODO: use environments to provide API url instead of this lol
   //apiUrl: 'http://localhost:3000';
+
+  //tmp: any = {};
+  authSubject = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) {
 
@@ -27,4 +30,38 @@ export class ApiService {
       'http://localhost:3000/games/' + gameID,
     );
   }
+
+  public signIn(username: string, password: string){
+    let header = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post('http://localhost:3000/account/login', {email: username, password: password}, {headers: header}).pipe(
+      tap(async (res) => {
+        //let uhh: any = res;
+        let tmp: any = res;
+        if(tmp.user){
+          console.log("API Service is setting TOKEN and USER in local storage...");
+          console.log(tmp);
+          localStorage.setItem("TOKEN", tmp.token);
+          localStorage.setItem("USER", tmp.user);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+  public signOut(){
+    localStorage.removeItem("TOKEN");
+    localStorage.removeItem("USER");
+    this.authSubject.next(false);
+  }
+  public register(){
+
+  }
+  public isAuthenticated(){
+    return this.authSubject.asObservable();
+  }
+
+  public getUsers(){
+    console.log('Token:', localStorage.getItem("TOKEN"));
+    return this.http.get('http://localhost:3000/account/users', {headers: {'Authorization': localStorage.getItem("TOKEN")}});
+  }
+
 }
