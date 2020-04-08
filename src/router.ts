26331bdb96eb2d3b35135169c77e98a49f975fc6
Jared Glaser <Jared@glaserfamily.net>
2020-04-08 16:52:22 -0400

    modified request stubs to be the proper type in the router config

--- src/router.ts
+++ src/router.ts
@@ -13,28 +13,28 @@ export class ApiRouter {
     // Creates the routes for this router and returns a populated router object
     public getRouter(): express.Router {
         //Handle account controller methods
-        this.router.get("/account/create", this.accountcontroller.CreateUser);
+        this.router.post("/account", this.accountcontroller.CreateUser);
         this.router.get("/account/authenticate", this.accountcontroller.AuthenticateUser);
-        this.router.get("/account/addfriend", this.accountcontroller.AddFriend);
-        this.router.get("/account/removefriend", this.accountcontroller.RemoveFriend);
+        this.router.post("/account/addfriend", this.accountcontroller.AddFriend);
+        this.router.delete("/account/removefriend", this.accountcontroller.RemoveFriend);
         this.router.get("/account/refreshfriends", this.accountcontroller.RefreshFriends);
         this.router.get("/account/requestphotos", this.accountcontroller.RequestPhotos);
-        this.router.get("/account/sendchat", this.accountcontroller.SendFriendChat);
+        this.router.post("/account/sendchat", this.accountcontroller.SendFriendChat);
         this.router.get("/account/refreshchat", this.accountcontroller.RefreshFriendChat);
 
         //Handle server controller methods
-        this.router.get("/server/create", this.servercontroller.CreateServer);
-        this.router.get("/server/join", this.servercontroller.JoinServer);
-        this.router.get("/server/leave", this.servercontroller.LeaveServer);
+        this.router.post("/server/create", this.servercontroller.CreateServer);
+        this.router.post("/server/join", this.servercontroller.JoinServer);
+        this.router.delete("/server/leave", this.servercontroller.LeaveServer);
         this.router.get("/server/refresh", this.servercontroller.RefreshServer);
         
         //Handle room controller methods
-        this.router.get("/room/create", this.roomcontroller.CreateRoom);
-        this.router.get("/room/sendchat", this.roomcontroller.SendRoomChat);
+        this.router.post("/room/create", this.roomcontroller.CreateRoom);
+        this.router.post("/room/sendchat", this.roomcontroller.SendRoomChat);
         this.router.get("/room/refreshchat", this.roomcontroller.RefreshRoomChat);
-        this.router.get("/room/joinvoice", this.roomcontroller.JoinRoomVoice);
+        this.router.post("/room/joinvoice", this.roomcontroller.JoinRoomVoice);
         this.router.get("/room/refreshvoice", this.roomcontroller.RefreshRoomVoice);
-        this.router.get("/room/joinvideo", this.roomcontroller.JoinRoomVideo);
+        this.router.post("/room/joinvideo", this.roomcontroller.JoinRoomVideo);
         this.router.get("/room/refreshvideo", this.roomcontroller.RefreshRoomVideo);
 
         return this.router;
